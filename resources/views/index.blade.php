3f77002e1444121946c318c25b43ebb484f764d4
esalasbauza <esalasbauza@gmail.com>
2024-12-09 20:28:18 +0100

    styling lists

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,24 +4,25 @@
 
 @section('content')
 
-<div>
     <div>
-        <a href="{{ route('tasks.create') }}">Create</a>
-    </div>
-    @forelse ( $tasks as $task )
-    <div>
-        <a href="{{ route('tasks.show', ['task' => $task->id ]) }}">{{ $task->title }}</a>
-    </div>
+        <nav class="mb-4">
+            <a href="{{ route('tasks.create') }}" class="link">Add Task!</a>
+        </nav>
+        @forelse ($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+                    @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+            </div>
 
-    @empty
-    <div>no hay tasks</div>
-    @endforelse
+        @empty
+            <div>no hay tasks</div>
+        @endforelse
 
-    @if ($tasks->count())
-    <nav>
-        {{ $tasks->links() }}
-    </nav>
-    @endif
-</div>
+        @if ($tasks->count())
+            <nav>
+                {{ $tasks->links() }}
+            </nav>
+        @endif
+    </div>
 
 @endsection
